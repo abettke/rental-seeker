2a71b4d24d75415de355e6efe02dc53b1a3bff46
Andrew Bettke <abettke@gmail.com>
2020-03-23 10:25:44 -0400

    Add serve static module for dishing up the react app from nestjs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database';
+import { ServeModule } from './serve.module';
 import { AuthenticationModule } from './auth';
 import { UserModule } from './user';
 import { RentalModule } from './rental';
@@ -7,6 +8,7 @@ import { RentalModule } from './rental';
 @Module({
   imports: [
     DatabaseModule,
+    ServeModule,
     AuthenticationModule,
     UserModule,
     RentalModule,
---
+++ src/serve.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { resolve } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '..', 'client/build'),
+      exclude: ['/api*'],
+    }),
+  ],
+})
+export class ServeModule {}
