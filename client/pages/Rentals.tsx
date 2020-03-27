6f3eb3e8cc04493ccf57ffd2ae2b3d7a18987bd0
Andrew Bettke <abettke@gmail.com>
2020-03-27 18:55:20 -0400

    Fix full height flex growing app-wide.

--- client/pages/Rentals.tsx
+++ client/pages/Rentals.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { TopBar } from '../components/TopBar';
 import { FilterBar } from '../components/FilterBar';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
 
 export const Rentals: React.FC = () => {
+  const classes = useStyles();
   return (
     <>
-      <TopBar />
-      <FilterBar />
+      <header>
+        <TopBar />
+        <FilterBar />
+      </header>
+      <Grid
+        container
+        component={'main'}
+        className={classes.root}
+      >
+        <Grid item xs={5}>List View</Grid>
+        <Grid item xs>Map View</Grid>
+      </Grid>
     </>
   )
 };
