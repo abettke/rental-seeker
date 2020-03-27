a160a10dadc46adeffa3741d20480887b595df2e
Andrew Bettke <abettke@gmail.com>
2020-03-27 12:28:31 -0400

    Add basic filter bar outline.

---
+++ client/components/FilterBar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    switch: {
+      display: 'flex',
+      alignItems: 'center',
+      '& .MuiTypography-caption': {
+        whiteSpace: 'nowrap',
+      },
+    },
+  }),
+);
+
+export const FilterBar: React.FC = () => {
+
+  const classes = useStyles();
+  return (
+    <AppBar position={'static'} color={'default'}>
+      <Toolbar color={'default'}>
+        <Grid container>
+          <Button>Filter 1</Button>
+          <Button>Filter 2</Button>
+          <Button>Filter 3</Button>
+        </Grid>
+        <div className={classes.switch}>
+          <Typography variant={'caption'}>Show Map</Typography>
+          <Switch />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
--- client/pages/Rentals.tsx
+++ client/pages/Rentals.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { TopBar } from '../components/TopBar';
+import { FilterBar } from '../components/FilterBar';
 
 export const Rentals: React.FC = () => {
   return (
-    <TopBar />
+    <>
+      <TopBar />
+      <FilterBar />
+    </>
   )
 };
