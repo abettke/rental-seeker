388a7fc5fd56a36c2dab63f4c24b0405372e8762
Andrew Bettke <abettke@gmail.com>
2020-03-28 12:30:42 -0400

    Protect auth routes from expired tokens.

--- client/routes/AuthRoute.tsx
+++ client/routes/AuthRoute.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { decode } from 'jsonwebtoken'
 import { Redirect, Route, RouteProps, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Routes } from './index';
@@ -6,8 +7,9 @@ import { Routes } from './index';
 export const AuthRoute: React.FC<RouteProps> = (props: RouteProps) => {
   const location = useLocation();
   const { auth } = useAuth();
+  const NOW = Math.round(new Date().getTime() / 1000);
 
-  return (auth?.accessToken ?
+  return (auth?.accessToken && decode(auth.accessToken)['exp'] > NOW ?
     <Route {...props} /> :
     <Redirect
       to={{
